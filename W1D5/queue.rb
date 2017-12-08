9c46a127a09b13ee46d15ae092d70587780770d5
locphan2207 <locphan2207@gmail.com>
2017-12-08 01:03:52 -0800

    uncomment

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -1,22 +1,18 @@
 class Queue
   attr_reader :array
   def initialize
-    # create ivar to store stack here!
     @array = []
   end
 
   def enqueue(el)
-    # adds an element to the stack
     @array << el 
   end
 
   def dequeue
-    # removes one element from the stack
     @array.shift
   end
 
   def show
-    # return a copy of the stack
     @array
   end
 end
