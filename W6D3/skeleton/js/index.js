44ba2a8686b603d50c466de6c0f20e41d32ea92b
Tan Loc Phan <locphan2207@gmail.com>
2018-01-17 00:12:49 -0800

    hw

---
+++ W6D3/skeleton/js/index.js
@@ -0,0 +1,17 @@
+console.log('Hello from the JavaScript console!');
+
+// Your AJAX request here
+$.ajax({
+  type: 'GET',
+  url: 'http:/api.openweathermap.org/data/2.5/weather?q=New+York&appid=c8a076e6676bbb38a2ac8046da15fae2',
+  success(weather) {
+    console.log('Got Newyork weather!');
+    console.log(weather);
+  },
+  error() {
+    console.error("An error occurred.");
+  },
+});
+
+// Add another console log here, outside your AJAX request
+console.log('IDK?');
