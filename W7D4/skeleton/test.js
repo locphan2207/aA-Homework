05ee70344b517b3489ae348986640a94a32b07c2
Tan Loc Phan <locphan2207@gmail.com>
2018-01-29 01:58:23 -0800

    w8

---
+++ W7D4/skeleton/test.js
@@ -0,0 +1,73 @@
+{
+  stories: {
+    byId: {
+      "2": {
+        id: "2",
+        author: "2",
+        img_url: "...",
+        rating: 3,
+        difficulty: "easy",
+        preparation_min: 15,
+        baking_min: 0,
+        resting_min: 0,
+        ingredients: ["3", "4", "30", "31", "10", "102"]
+        text: "...",
+        comments: ["3", "5"]
+      },
+      "3": {
+        id: "3",
+        author: "9",
+        img_url: "...",
+        rating: 3,
+        difficulty: "easy",
+        preparation_min: 15,
+        baking_min: 0,
+        resting_min: 0,
+        ingredients: ["3", "4", "2", "1", "9", "10"]
+        text: "...",
+        comments: ["1", "2"]
+      }
+    },
+    allIds: ["2", "3"]
+  },
+  comments: {
+    byId: {
+      "1": {
+        id: "1",
+        user: "3",
+        body: "...",
+      },
+      "2": {
+        id: "2",
+        user: "3",
+        body: "...",
+      },
+      "3": {
+        id: "3",
+        user: "1",
+        body: "...",
+      },
+      "5": {
+        id: "5",
+        user: "10",
+        body: "...",
+      }
+    },
+    allIds: ["1", "2", "3", "5"]
+  },
+  users: {
+    byId: {
+      "3": {
+        id: "3",
+        username: "...",
+        pic_url: "..."
+      },
+      "1": {
+        id: "1",
+        username: "...",
+        pic_url: "..."
+      },
+    },
+    allIds: ["1", "3",...]
+  }
+}
